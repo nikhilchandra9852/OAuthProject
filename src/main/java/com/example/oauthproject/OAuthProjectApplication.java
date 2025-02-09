99d0b416fd0b6e1981aa716d3717176e0d45b673
Nikhil <nikhilchandrapanchanapu@gmail.com>
2025-02-09 19:08:32 +0530

    Initial commit

---
+++ src/main/java/com/example/oauthproject/OAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauthproject/OAuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
